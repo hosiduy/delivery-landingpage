10de1d0c22f331201eda31760fbc38f4f162e4da
admin@NgocQuynh <LQuynh09>
2024-07-13 17:59:41 +0700

    update phuong tien

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    var detailButtons = document.querySelectorAll('.detailButton');
-    var overlay = document.getElementById('overlay');
-    var detailContent = document.getElementById('detailContent');
-    var closeButton = document.getElementById('closeButton');
 
-    detailButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var detailText = button.getAttribute('data-detail');
-            detailContent.textContent = detailText;
-            overlay.style.display = 'flex';
+document.addEventListener('DOMContentLoaded', function()
+{
+    var zalobutton = document.querySelector('.btn-zalo');
+    var callbutton = document.querySelector('.btn-call');
+    var button = document.querySelector('.btn.btn-primary.btn-lg.mt-3');
+    var successbtn = document.querySelector('.btn-success');
+    if (zalobutton)
+    {
+        zalobutton.addEventListener('click', function() 
+        {
+            var zaloChatUrl = 'https://zalo.me/0915549594?';
+            window.open(zaloChatUrl);
         });
-    });
-
-    closeButton.addEventListener('click', function() {
-        overlay.style.display = 'none';
-    });
-
-    // Close the overlay when clicking outside of the content area
-    overlay.addEventListener('click', function(event) {
-        if (event.target === overlay) {
-            overlay.style.display = 'none';
+    }
+    if (callbutton)
+        {
+            callbutton.addEventListener('click', function() 
+            {
+                var phoneNumber = '0915549594';
+                var telUri = 'tel:' + phoneNumber;
+    
+                // Mở cuộc gọi điện thoại
+                window.location.href = telUri;
+            });
         }
-    });
+        if (button)
+            {
+                button.addEventListener('click', function() 
+                {
+                    var zaloChatUrl = 'https://zalo.me/0915549594?text=Hello';
+                    window.open(zaloChatUrl);
+                });
+            }
+
+                if (successbtn)
+                    {
+                        successbtn.addEventListener('click', function() 
+                        {
+                            var zaloChatUrl = 'https://zalo.me/0915549594?text=Hello';
+                            window.open(zaloChatUrl);
+                        });
+                    }
 });
